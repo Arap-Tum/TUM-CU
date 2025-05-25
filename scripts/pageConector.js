e55a7510244029c156441a1fb0b64b597fd88856
Arap-Tum <tumkibet32@gmail.com>
2025-05-25 08:18:22 +0300

    Implement footer section with newsletter validation and social links; refactor footer styles for improved layout

--- scripts/pageConector.js
+++ scripts/pageConector.js
@@ -1,4 +1,5 @@
 import { openCloseSidebar } from "./sidebar.js";
+import { newsletterValidation } from "./footer.js";
 
 // connect pages 
 export function  connectSections() {
@@ -7,10 +8,18 @@ export function  connectSections() {
     .then(data => {
        document.querySelector('.header').innerHTML = data;
        openCloseSidebar()
-       console.log(document.querySelector('.header-learge'))
+    //    console.log(document.querySelector('.header-learge'))
     })
     .catch(error => {
         console.log(error)
+    });
+
+    fetch('./includes/footer.html')
+    .then(res => res.text())
+    .then(data => {
+        document.querySelector('.footer').innerHTML =data
+        newsletterValidation()
+
     })
 }
 
