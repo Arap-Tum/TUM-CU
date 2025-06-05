1f534d043eb1dc2613d23b926702585c24eab3d1
Arap-Tum <tumkibet32@gmail.com>
2025-06-05 19:05:45 +0300

    feat: Add Eteams page styles and enhance layout with dynamic rendering

--- modules/eteams.js
+++ modules/eteams.js
@@ -5,9 +5,11 @@
 import { connectSections } from "../scripts/pageConector.js";
 import { openCloseForm } from "../function/formAction.js";
 import { renderEteamsCards } from "../utils/renderEteams.js";
+import { modalDisplayOnScroll } from "../function/scrollEffectHeader.js";
 // Conect the page with the header and footer
 connectSections();
 openCloseForm();
+modalDisplayOnScroll();
 
 //Eteams dynamically added
 renderEteamsCards();
