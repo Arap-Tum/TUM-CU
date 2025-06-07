9f345ddb26e4386189f84ce2565fdd1ff70c5f14
Arap-Tum <tumkibet32@gmail.com>
2025-06-07 19:50:12 +0300

    feat: Remove unused header and footer files, and add new header and footer structure for improved navigation and layout

--- scripts/pageConector.js
+++ scripts/pageConector.js
@@ -1,31 +1,29 @@
 import { openCloseSidebar } from "./sidebar.js";
 import { scrollEfect } from "../function/scrollEffectHeader.js";
 
-// connect pages 
-export function  connectSections() {
-    const currentPath = window.location.pathname;
-    const isInPagesFolder = currentPath.includes('/pages/');
+// connect pages
+export function connectSections() {
+  const currentPath = window.location.pathname;
+  const isInPagesFolder = currentPath.includes("/pages/");
 
-    const basePath = isInPagesFolder ? '../includes/' : './includes/';
-    
-    fetch(`${basePath}header.html`)
-    .then(res => res.text())
-    .then(data => {
-       document.querySelector('.header').innerHTML = data;
-       openCloseSidebar()
-       scrollEfect()
-    //    console.log(document.querySelector('.header-learge'))
+  const basePath = isInPagesFolder ? "../pages/" : "./pages/";
+
+  fetch(`${basePath}header.html`)
+    .then((res) => res.text())
+    .then((data) => {
+      document.querySelector(".header").innerHTML = data;
+      openCloseSidebar();
+      scrollEfect();
+      //    console.log(document.querySelector('.header-learge'))
     })
-    .catch(error => {
-        console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
 
-    fetch(`${basePath}footer.html`)
-    .then(res => res.text())
-    .then(data => {
-        document.querySelector('.footer').innerHTML =data
-        // newsletterValidation()
-
-    })
+  fetch(`${basePath}footer.html`)
+    .then((res) => res.text())
+    .then((data) => {
+      document.querySelector(".footer").innerHTML = data;
+      // newsletterValidation()
+    });
 }
-
