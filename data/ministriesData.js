c1a50eb5ddefdc00414742e46537b1a4c168f100
Jelly2252 <angelamaina001@gmail.com>
2025-06-07 20:04:38 +0300

    Add files via upload

--- data/ministriesData.js
+++ data/ministriesData.js
@@ -58,19 +58,105 @@ export const ministries = [
     },
     {
         id: 5,
-        name: "Media",
-        description: "",
+        name: "Media Ministry",
+        description: "capturing every powerful moment in God's presence ,preserving memories, enhancing worship, and telling the church's story through creative visuals and sound.",
         image: "", 
         responsibilities: [
             "Projection",
+            "Photography",
+            "Live Streaming",
+            "video editing",
+            "Graphic designing",
             // Add more responsibilities
             
         ],
         leaderPhone: "+2547",
         leaderName: "Ministry Leader"
-    }
+    },
 
-    // Add al the other ministries here
+    // Add all the other ministries here
     // DO NOT ADD IMAGES 
+    {
+        id: 6,
+        name: "Creative Ministry",
+        description: " Ministering using our God-given talents, to brings ideas to life through art, design, and innovation all for the glory of God and to uplift the church experience.",
+        image: "", 
+        responsibilities: [
+            "Dancing",
+            "Acting",
+            "Spoken word",
+            "Poetic Plays",
+            "Singing",
+            "Story Telling",
+            // Add more responsibilities
+            
+        ],
+        leaderPhone: "+2547",
+        leaderName: "Ministry Leader"
+    },
+      {
+        id: 7,
+        name: "Instrumentalist Ministry",
+        description: "Using the power of sound with skilled hands and passionate hearts to bring worship to life,playing every note with passion to glorify God and lead the church in praise.",
+        image: "", 
+        responsibilities: [
+            "instrument playing",
+            "Venue Set Ups",
+            "Instruments maintainance",
+            "Support the worship team  ",
+           // Add more responsibilities
+            
+        ],
+        leaderPhone: "+2547",
+        leaderName: "Ministry Leader"
+    },
+    {
+        id: 8,
+        name: "Ushering Ministry",
+        description: "Welcoming everyone with warm smiles and open hearts,creating a friendly and organized environment making sure every visitor and member feels valued and comfortable from the moment they arrive.",
+        image: "", 
+        responsibilities: [
+            "Welcoming visitors",
+            "Tithe's and Offering Assistance",
+            "Communion Assistance",
+            "Maintaining order in Church",
+            "Emergency support",
+            // Add more responsibilities
+            
+        ],
+        leaderPhone: "+2547",
+        leaderName: "Ministry Leader"
+    },
+    {
+        id: 9,
+        name: "Catering Ministry",
+        description: "Using their passion for hospitality and hand skills to serve delicious meals that bring the church family together,ensuring everyone is well-fed and cared for during events, creating a warm and welcoming atmosphere through food and fellowship",
+        image: "", 
+        responsibilities: [
+            "food preparation and service",
+            "Service coordination",
+            "Kitchen supplies management",
+            "Event Coordination",
+            // Add more responsibilities
+            
+        ],
+        leaderPhone: "+2547",
+        leaderName: "Ministry Leader"
+    },
+     {
+        id: 10,
+        name: "Technician Ministry",
+        description: "Using their technical skills to keep all church equipment running smoothly,creating the best environment for services and events.",
+        image: "", 
+        responsibilities: [
+            "Sound Engineering",
+            "Equipment Maintenance",
+            "Venue Set Ups",
+            // Add more responsibilities
+            
+        ],
+        leaderPhone: "+2547",
+        leaderName: "Ministry Leader"
+    },
 ];
 
